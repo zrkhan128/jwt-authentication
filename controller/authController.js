2e65ff9896dc83eeabbed311785e120893835f49
Zeeshan Rehmat <zee.ktk97@gmail.com>
2022-10-11 11:53:50 +0500

    JWT authentication boiler plate

---
+++ controller/authController.js
@@ -0,0 +1,37 @@
+app.post("/user/generateToken", (req, res) => {
+	// Validate User Here
+	// Then generate JWT Token
+
+	let jwtSecretKey = process.env.JWT_SECRET_KEY;
+	let data = {
+		time: Date(),
+		userId: 12,
+	}
+
+	const token = jwt.sign(data, jwtSecretKey);
+
+	res.send(token);
+});
+app.get("/user/validateToken", (req, res) => {
+	// Tokens are generally passed in the header of the request
+	// Due to security reasons.
+
+	let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+	let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+	try {
+		const token = req.header(tokenHeaderKey);
+
+		const verified = jwt.verify(token, jwtSecretKey);
+		if(verified){
+			return res.send("Successfully Verified");
+		}else{
+			// Access Denied
+			return res.status(401).send(error);
+		}
+	} catch (error) {
+		// Access Denied
+		return res.status(401).send(error);
+	}
+});
+
